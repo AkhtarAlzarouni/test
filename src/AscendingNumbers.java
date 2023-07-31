8db84477dd9f56c0ce3ade0adc8193d8fd86d203
Akhtar Alzrouni <138444303+AkhtarAlzarouni@users.noreply.github.com>
2023-07-31 15:53:54 +0400

    Add files via upload

---
+++ src/AscendingNumbers.java
@@ -0,0 +1,42 @@
+public class AscendingNumbers {
+    public static void main(String[] args) {
+        int[] arrayX={5,7,3,6,1};
+
+        Ascending(arrayX);
+        System.out.println("\nArray after sorting:");
+        printArray(arrayX);
+
+    }
+
+    public static void Ascending(int[] arr) {
+        int n = arr.length;
+        boolean sw;
+
+        for (int i = 0; i < n - 1; i++) {
+            sw = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    sw = true;
+                }
+            }
+
+
+            if (!sw) {
+                break;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    }
+
